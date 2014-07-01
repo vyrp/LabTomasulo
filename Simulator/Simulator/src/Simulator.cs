4102235caf65967cff012e5671d26657deb85f1d
vyrp <felipe.croata@gmail.com>
2014-07-01 08:13:52 -0300

    Cleaning usings and references

--- Simulator/Simulator/src/Simulator.cs
+++ Simulator/Simulator/src/Simulator.cs
@@ -1,13 +1,7 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
 using System.Linq;
-using System.Text;
 using System.IO;
-using System.Threading;
-using System.Threading.Tasks;
 using LabTomasulo.Arquitetura;
 
 using SI = System.Collections.Generic.KeyValuePair<LabTomasulo.StationType, int>;
